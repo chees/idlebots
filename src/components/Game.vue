273606dd4a02e66e88db3b87054c60e637187fd8
Christiaan Hees <mail@chees.info>
2018-01-24 16:05:34 +0100

    Buy bots using materials

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -2,36 +2,64 @@
   <div class="game">
     <h1>Idle Bots</h1>
 
-    Materials: {{materials}}
+    Materials: {{ materials.toString() }}
+    ({{ rate.toString() }}/s)
 
-    <div v-for="(bot, index) in bots" :key="bot.name">
-      {{ bot.name }}
-      <button>Buy 1</button>
-      <button>Buy Max</button>
+    <div v-for="bot in bots" :key="bot.name" class="bot">
+      {{ bot.owned.toString() }} {{ bot.name }}
+      <div>
+        <button @click="buy(bot)" :disabled="materials.lt(bot.cost)">Buy 1 ({{ bot.cost }})</button>
+        <button @click="buy(bot)" :disabled="materials.lt(bot.cost)">Buy Max</button>
+      </div>
     </div>
 
   </div>
 </template>
 
 <script>
+import Big from 'big.js'
+
 export default {
   name: 'game',
   data () {
     return {
-      materials: 10,
+      materials: new Big(100),
+      rate: new Big(0),
       bots: [
-        { name: 'Nanobot' },
-        { name: 'Minibot' }
+        { name: 'Nanobots', owned: new Big(0), cost: 100, generates: 1 },
+        { name: 'Minibots', owned: new Big(0), cost: 1000, generates: 10 }
       ]
     }
+  },
+  methods: {
+    tick: function () {
+      const sleep = 100
+      const before = this.materials
+      for (const bot of this.bots) {
+        this.materials = this.materials.plus(bot.owned.times(bot.generates))
+      }
+      this.rate = this.materials.minus(before).times(1000 / sleep)
+      setTimeout(this.tick, sleep)
+    },
+    buy: function (b) {
+      this.materials = this.materials.minus(b.cost)
+      b.owned = b.owned.plus(1)
+    }
+  },
+  created: function () {
+    this.tick()
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .game {
   margin: 0 10px;
+}
+
+.bot {
+  display: flex;
+  justify-content: space-between;
 }
 
 button {
